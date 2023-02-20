839f1a529f503dcb5010d616d7fdc6ffa0927e6c
philipp-ding <philipp.dingi@gmail.com>
2023-02-20 21:01:46 +0100

    ELIZA version 0.1: possibility for psychiatrist and some small talk communication in the terminal/ cmd

---
+++ rules/small_talk_rules_ELIZA.py
@@ -0,0 +1,16 @@
+small_talk = [
+    [r'(.*) weather (.*)',
+     ["What do you think about the weather today?",
+      "Do you enjoy the weather today?"]],
+    [r'I like (.*)',
+     ["Why do you like {0}?",
+      "That's great, what else do you like?"]],
+    [r'I dislike (.*)',
+     ["Why don't you like {0}?",
+      "What do you like instead?"]],
+    [r'(.*) raining (.*)',
+     ["Do you like rain or do you prefer sunny weather?",
+      "How does the rain affect you?"]],
+    [r'(.*) sunny (.*)',
+     ["Do you enjoy sunny weather?",
+      "What do you like to do on sunny days?"]]]
